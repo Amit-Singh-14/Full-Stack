c6bca60ccbb4a66118a8dd8904be7611527c280c
Axcel014 <amitasdf246@gmail.com>
2023-06-23 10:47:27 +0530

    auth level 6 express-session, passport and cookies

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,14 +3,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-
-const saltRounds = 5;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//  adding session to our app
+app.use(
+  session({
+    secret: "amitsingh.",
+    resave: false,
+    saveUninitialized: false,
+    expires: false,
+  })
+);
+
+// adding passport to our app
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB").then(() => {
   console.log("db connected");
@@ -21,7 +36,20 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
+/*
+adding passportlocalmongoose to schema 
+it is for hashing and salting the password
+*/
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userSchema);
+
+//createStrategy as static method to your schema.
+// The createStrategy is responsible to setup passport-local LocalStrategy with the correct options.
+passport.use(User.createStrategy());
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -33,33 +61,48 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register", (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds).then((hash) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-    newUser
-      .save()
-      .then(() => {
-        console.log("new user added");
-        res.render("secrets");
-      })
-      .catch((err) => console.log(err.message));
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    console.log("authenticated");
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) console.log(err);
   });
+  res.redirect("/");
+});
+
+app.post("/register", (req, res) => {
+  User.register({ username: req.body.username }, req.body.password)
+    .then((user) => {
+      console.log(user);
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/register");
+    });
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
 
-  User.findOne({ email: username }).then((result) => {
-    console.log(result);
-    if (result) {
-      if (bcrypt.compare(password, result.password)) res.render("secrets");
-      else res.send("invalid username or apssword");
-    } else {
-      res.send("no such user");
+  req.login(user, (err) => {
+    if (err) console.log(err);
+    else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
     }
   });
 });
